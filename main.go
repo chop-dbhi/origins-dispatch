dce01457b96875f6991c7e9f24f2afabdb0b5f12
Byron Ruth <b@devel.io>
2015-01-12 07:09:26 -0500

    Main command prints usage, update command descriptions
    Signed-off-by: Byron Ruth <b@devel.io>

--- main.go
+++ main.go
@@ -6,25 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.9.0"
-
-var mainCmd = &cobra.Command{
-	Use:   "origins-webhooks-service",
-	Short: "Origins Webhooks service",
-	Long:  "A service that broadcasts event payloads to registered webhooks.",
-	Run: func(cmd *cobra.Command, args []string) {
-
-	},
-}
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version",
-	Long:  "The version of the Origins Webhooks service.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
-	},
-}
+const version = "0.1.0"
 
 var (
 	// Global
@@ -36,16 +18,38 @@ var (
 	serveNeo4j string
 )
 
+// The main command describes the service and defaults to printing the
+// help message.
+var mainCmd = &cobra.Command{
+	Use:   "origins-dispatch",
+	Short: "Event dispatch service for Origins.",
+	Long: `An HTTP service that consumes events produced by Origins and dispatches
+them to subscribers.
+
+Learn more about Origins: https://github.com/chop-dbhi/origins/`,
+}
+
+// The version command prints this service.
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version.",
+	Long:  "The version of the origins-dispatch service.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version)
+	},
+}
+
+// The serve command runs the service.
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "Runs the service",
-	Long:  "Runs the service",
+	Short: "Runs the service.",
+	Long:  "The serve command runs the service.",
 	Run: func(cmd *cobra.Command, args []string) {
 		serve()
 	},
 }
 
-func init() {
+func main() {
 	// Main flags
 	mainFlags := serveCmd.PersistentFlags()
 	mainFlags.BoolVar(&debug, "debug", false, "Turn on debugging.")
@@ -59,8 +63,6 @@ func init() {
 	// Register subcommands
 	mainCmd.AddCommand(versionCmd)
 	mainCmd.AddCommand(serveCmd)
-}
 
-func main() {
 	mainCmd.Execute()
 }
